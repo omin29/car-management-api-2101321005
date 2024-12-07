130d522564f5d9299b1fbb19c4d216d90107bed8
omin29 <onur_solak@abv.bg>
2024-12-07 19:43:29 +0200

    Initial Setup
    * Prepared base project with Spring Initializr
    * Hooked up the project with EGit

---
+++ carmanagement/src/main/java/pu/fmi/wsp/hw/carmanagement/CarManagementApiApplication.java
@@ -0,0 +1,13 @@
+package pu.fmi.wsp.hw.carmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarManagementApiApplication.class, args);
+	}
+
+}
---
+++ carmanagement/src/test/java/pu/fmi/wsp/hw/carmanagement/CarManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pu.fmi.wsp.hw.carmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
